ca54b458c9b911df54547ebf895551148b3f27cc
prince <princepatel.vis15@gmail.com>
2025-09-04 23:55:29 -0400

    Changing paths in Script
    last minutes changes in paths

--- miner/myScript/coin.sh
+++ miner/myScript/coin.sh
@@ -3,7 +3,7 @@
 ## Prints all words in provided dictionary
 ## (you might want to find a bigger dictionary)
 for number in $(seq 0 999); do
-       for word in $(cat /home/prince/hashing/data/dictionary2); do
+       for word in $(cat /home/prince/git/lab-1-hashing-Prince-code-hub/miner/myScript/data/dictionary ); do ## I just change dictionary manually 
 	combo="$number$word"
 	printf "$combo: "
 	echo -n $combo | sha256sum
